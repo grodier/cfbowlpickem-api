6884f51e9e37759ecdfa45cf3db90fe97ca83b7a
George.Rodier <grodier3@gmail.com>
2019-04-24 15:13:28 -0400

    Set up some relationship queries

--- src/index.js
+++ src/index.js
@@ -10,11 +10,21 @@ const schema = gql`
     me: User
     user(id: ID!): User
     users: [User!]
+
+    messages: [Message!]!
+    message(id: ID!): Message!
   }
 
   type User {
     id: ID!
     username: String!
+    messages: [Message!]
+  }
+
+  type Message {
+    id: ID!
+    text: String!
+    user: User!
   }
 `;
 
@@ -22,30 +32,50 @@ let users = {
   1: {
     id: '1',
     username: 'George Rodier',
+    messageIds: [1],
   },
   2: {
     id: '2',
     username: 'Dave Davids',
+    messageIds: [2],
   },
 };
 
-const me = users[1];
+let messages = {
+  1: {
+    id: '1',
+    text: 'Hello World',
+    userId: '1',
+  },
+  2: {
+    id: '2',
+    text: 'By World',
+    userId: '2',
+  },
+};
 
 const resolvers = {
   Query: {
-    me: () => me,
-    user: (parent, { id }) => {
-      return users[id];
-    },
-    users: () => {
-      return Object.values(users);
-    },
+    me: (parent, args, { me }) => me,
+    user: (parent, { id }) => users[id],
+    users: () => Object.values(users),
+    messages: () => Object.values(messages),
+    message: (parent, { id }) => messages[id],
+  },
+  Message: {
+    user: message => users[message.userId],
+  },
+  User: {
+    messages: user => Object.values(messages).filter(message => message.userId === user.id),
   },
 };
 
 const server = new ApolloServer({
   typeDefs: schema,
   resolvers,
+  context: {
+    me: users[1],
+  },
 });
 
 server.applyMiddleware({

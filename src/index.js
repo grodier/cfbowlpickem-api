be2ee1f56cb12f2084367edd00874f836ab18b91
George.Rodier <grodier3@gmail.com>
2019-04-24 15:44:20 -0400

    Add mutation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import { ApolloServer, gql } from 'apollo-server-express';
+import uuidv4 from 'uuid/v4';
 
 const app = express();
 app.use(cors());
@@ -13,6 +14,11 @@ const schema = gql`
 
     messages: [Message!]!
     message(id: ID!): Message!
+  }
+
+  type Mutation {
+    createMessage(text: String!): Message!
+    deleteMessage(id: ID!): Boolean!
   }
 
   type User {
@@ -61,6 +67,26 @@ const resolvers = {
     users: () => Object.values(users),
     messages: () => Object.values(messages),
     message: (parent, { id }) => messages[id],
+  },
+  Mutation: {
+    createMessage: (parent, { text }, { me }) => {
+      const id = uuidv4();
+      const message = { id, text, userId: me.id };
+      messages[id] = message;
+      users[me.id].messageIds.push(id);
+      return message;
+    },
+    deleteMessage: (parent, { id }) => {
+      const { [id]: message, ...otherMessages } = messages;
+
+      if (!message) {
+        return false;
+      }
+
+      messages = otherMessages;
+
+      return true;
+    },
   },
   Message: {
     user: message => users[message.userId],
